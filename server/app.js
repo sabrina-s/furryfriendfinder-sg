f3b61c28c27b386924092a948eaca0c4025852f0
Sabrina Sulong <tesmemepasla@gmail.com>
2021-02-07 17:36:19 +0800

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express");
+});
+
+module.exports = app;
