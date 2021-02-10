b176c00838f6c4b85e08ecf342c0fe0797e67de5
Sabrina Sulong <tesmemepasla@gmail.com>
2021-02-10 11:45:36 +0800

    Extract currentUserHook into useCurrentUserHook

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import Navbar from "./containers/Navbar/Navbar";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import RegisterPage from "./containers/RegisterPage";
 import LoginPage from "./containers/LoginPage";
 import { UserContext } from "./context/User";
-import axios from "axios";
-import { ME_API } from "./constants/api";
+import { useCurrentUserHook } from "./hooks/useCurrentUserHook";
 
 function App() {
-  const [currentUser, setCurrentUser] = useState();
-
-  useEffect(() => {
-    axios
-      .get(ME_API, { withCredentials: true })
-      .then((user) => setCurrentUser(user.data))
-      .catch(console.error);
-  }, []);
+  const { currentUser, setCurrentUser } = useCurrentUserHook();
 
   return (
     <div className="App">
---
+++ client/src/hooks/useCurrentUserHook.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import { ME_API } from "../constants/api";
+import axios from "axios";
+
+export const useCurrentUserHook = () => {
+  const [currentUser, setCurrentUser] = useState();
+
+  useEffect(() => {
+    axios
+      .get(ME_API, { withCredentials: true })
+      .then((user) => setCurrentUser(user.data))
+      .catch((err) => console.error("Not logged in."));
+  }, []);
+
+  return {
+    currentUser,
+    setCurrentUser,
+  };
+};
