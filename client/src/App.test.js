57b36a46c4c14e0ca62d6cee74ec6c58074fd2a6
Sabrina Sulong <tesmemepasla@gmail.com>
2021-02-10 12:17:18 +0800

    Add RTL tests for App for no currentUser case

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -7,4 +7,21 @@ describe("App", () => {
     const navbar = screen.getByTestId("navbar");
     expect(navbar).toBeInTheDocument();
   });
+
+  describe("no currentUser", () => {
+    it("should display login and register links", () => {
+      render(<App />);
+      const loginLink = screen.getByText(/login/i);
+      const registerLink = screen.getByText(/register/i);
+      expect(loginLink).toBeInTheDocument();
+      expect(registerLink).toBeInTheDocument();
+    });
+
+    it("should not display logout link", () => {
+      render(<App />);
+      const logoutLink = screen.queryByText(/logout/i);
+
+      expect(logoutLink).toBeNull();
+    });
+  });
 });
