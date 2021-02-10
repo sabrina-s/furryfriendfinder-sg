2038fb4bb71383453f998309fb8422ead7069bb4
Sabrina Sulong <tesmemepasla@gmail.com>
2021-02-10 14:30:45 +0800

    Add route for POST /dogs

--- server/routes/dogs.route.js
+++ server/routes/dogs.route.js
@@ -1,3 +1,4 @@
+const _ = require("lodash");
 const express = require("express");
 const Dog = require("../models/dog.model");
 const router = express.Router();
@@ -5,6 +6,26 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   const dogs = await Dog.find({});
   res.status(200).json(dogs);
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const dog = new Dog(
+      _.pick(req.body, [
+        "name",
+        "gender",
+        "description",
+        "hdbApproved",
+        "available",
+        "image",
+      ])
+    );
+    await dog.save();
+
+    res.status(200).json({ message: `${dog.name} added successfully!` });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
--- server/routes/dogs.route.test.js
+++ server/routes/dogs.route.test.js
@@ -28,12 +28,36 @@ describe("Users", () => {
     await Dog.create(dogs);
   });
 
-  describe("GET /", () => {
+  describe("GET /dogs", () => {
     it("should retrieve all dogs in the system", async () => {
       const response = await request(app).get("/dogs");
 
       expect(response.status).toBe(200);
       expect(response.body.length).toBe(2);
+    });
+  });
+
+  describe("POST /dogs", () => {
+    it("should create new dog if fields are valid", async () => {
+      const dog = {
+        name: "dog",
+        gender: "female",
+        description: "dog",
+        hdbApproved: false,
+        available: true,
+      };
+
+      const response = await request(app).post("/dogs").send(dog);
+
+      expect(response.status).toBe(200);
+    });
+
+    it("should throw error if required fields are missing", async () => {
+      const dog = { name: "dog" };
+
+      const response = await request(app).post("/dogs").send(dog);
+
+      expect(response.status).toBe(500);
     });
   });
 });
