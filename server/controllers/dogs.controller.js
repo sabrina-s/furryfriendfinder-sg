27cd406daa2bbbf2174a6cf9be87840f0f3b3901
Sabrina Sulong <tesmemepasla@gmail.com>
2021-03-08 15:54:02 +0800

    Extract main logic from dogs.route to dogs.controller

---
+++ server/controllers/dogs.controller.js
@@ -0,0 +1,79 @@
+const Dog = require("../models/dog.model");
+const { pick } = require("lodash");
+
+const getDogs = async (req, res, next) => {
+  try {
+    const dogs = await Dog.find({});
+    res.status(200).json(dogs);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const addDog = async (req, res, next) => {
+  try {
+    const dog = new Dog(
+      pick(req.body, [
+        "name",
+        "gender",
+        "description",
+        "hdbApproved",
+        "available",
+        "image",
+      ])
+    );
+    await dog.save();
+
+    res.status(200).json({ dog, message: `${dog.name} added successfully!` });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getDogById = async (req, res, next) => {
+  try {
+    const dog = await Dog.findById(req.params.id, ["-__v"]);
+    res.status(200).json(dog);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const updateDogById = async (req, res, next) => {
+  try {
+    const dog = await Dog.findByIdAndUpdate(
+      req.params.id,
+      pick(req.body, [
+        "name",
+        "gender",
+        "description",
+        "hdbApproved",
+        "available",
+        "image",
+      ]),
+      { new: true }
+    );
+
+    res.status(200).json({ message: `${dog.name} updated successfully!` });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const deleteDogById = async (req, res, next) => {
+  try {
+    const dog = await Dog.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({ message: `${dog.name} deleted successfully!` });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  getDogs,
+  addDog,
+  getDogById,
+  updateDogById,
+  deleteDogById,
+};
--- server/routes/dogs.route.js
+++ server/routes/dogs.route.js
@@ -1,73 +1,19 @@
-const _ = require("lodash");
+const { pick } = require("lodash");
 const express = require("express");
 const Dog = require("../models/dog.model");
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
 const router = express.Router();
+const ctrl = require("../controllers/dogs.controller");
 
-router.get("/", async (req, res) => {
-  const dogs = await Dog.find({});
-  res.status(200).json(dogs);
-});
+router.get("/", ctrl.getDogs);
 
-router.post("/", [auth.required, admin], async (req, res, next) => {
-  try {
-    const dog = new Dog(
-      _.pick(req.body, [
-        "name",
-        "gender",
-        "description",
-        "hdbApproved",
-        "available",
-        "image",
-      ])
-    );
-    await dog.save();
+router.post("/", [auth.required, admin], ctrl.addDog);
 
-    res.status(200).json({ dog, message: `${dog.name} added successfully!` });
-  } catch (error) {
-    next(error);
-  }
-});
+router.get("/:id", ctrl.getDogById);
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    const dog = await Dog.findById(req.params.id, ["-__v"]);
-    res.status(200).json(dog);
-  } catch (error) {
-    next(error);
-  }
-});
+router.put("/:id", [auth.required, admin], ctrl.updateDogById);
 
-router.put("/:id", [auth.required, admin], async (req, res, next) => {
-  try {
-    const dog = await Dog.findByIdAndUpdate(
-      req.params.id,
-      _.pick(req.body, [
-        "name",
-        "gender",
-        "description",
-        "hdbApproved",
-        "available",
-        "image",
-      ]),
-      { new: true }
-    );
-
-    res.status(200).json({ message: `${dog.name} updated successfully!` });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.delete("/:id", [auth.required, admin], async (req, res, next) => {
-  try {
-    const dog = await Dog.findByIdAndDelete(req.params.id);
-
-    res.status(200).json({ message: `${dog.name} deleted successfully!` });
-  } catch (error) {
-    next(error);
-  }
-});
+router.delete("/:id", [auth.required, admin], ctrl.deleteDogById);
 
 module.exports = router;
